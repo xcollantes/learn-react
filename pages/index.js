7f61f098104abbbb7b0664f62a3d19cdb4ebfa68
xcollantes <collantes.xavier@gmail.com>
2020-04-19 01:09:11 -0700

    API

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,7 @@
 import Link from 'next/link';
 import PropLayout from '../comps/PropLayout';
+import fetch from 'isomorphic-unfetch';
 
-const IndexThe = () => {
-    
-  <PropLayout>
-    <p>Content via Layout</p>
-  </PropLayout>
-
-};
-
-
-const Page = () => {
-  <p>Content via Higher Order</p>;
-};
 
 const PostLink = props => (
   <li>
@@ -22,16 +11,47 @@ const PostLink = props => (
   </li>
 );
 
+// 
+// export default function Blog() {
+//   return (
+//     <PropLayout>
+//       <h1>Bloggy</h1>
+//       <ul>
+// 	<PostLink id="Hello Next" />
+// 	<PostLink id="Learn Next JS is dope" />
+// 	<PostLink id="Deploy the apps" />
+//       </ul>
+//     </PropLayout>
+//   );
+// }
 
-export default function Blog() {
-  return (
-    <PropLayout>
-      <h1>Bloggy</h1>
-      <ul>
-	<PostLink id="Hello Next" />
-	<PostLink id="Learn Next JS is dope" />
-	<PostLink id="Deploy the apps" />
-      </ul>
-    </PropLayout>
-  );
+const Index = props => (
+  <PropLayout>
+    <h1>Batmans</h1>
+    <ul>
+      {props.shows.map(
+        show => (
+          <li key={show.id}>
+            <Link href="/p/[id]" as={`/p/${show.id}`}>
+              <a>{show.name}</a>
+            </Link>
+          </li>
+        )
+      )}
+    </ul>
+  </PropLayout>
+);
+
+Index.getInitialProps = async function() {
+  const response = await fetch('https://api.tvmaze.com/search/shows?q=batman');
+  const data = await response.json();
+
+  console.log(`Show data recieved. Count: ${data.length}`);
+  console.log({data});
+  console.log( {shows: data.map(entry => entry.show)} );
+  return { shows: data.map(entry => entry.show ) 
+  };
+
 };
+
+export default Index;
