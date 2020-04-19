be17553e5e7d9cdb6a53bde5130f6a04cda8701b
xcollantes <collantes.xavier@gmail.com>
2020-04-18 22:24:37 -0700

    move to different repo

---
+++ comps/HigherOrder.js
@@ -0,0 +1,18 @@
+import Header from './Header';
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD',
+};
+
+const HigherOrderLayout = Page => {
+  return () => (
+    <div style={layoutStyle}>
+      <Header />
+      <Page />
+    </div>
+  );
+};
+
+export default HigherOrderLayout;
